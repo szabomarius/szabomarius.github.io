4739eaa32e49111dccd355b02c6775b6605d95c0
Szabo Marius <szabomarius@gmail.com>
2014-04-13 17:07:19 +0300

    Coffe here, Made the devices fade in our out depening if the slided project is responsive or not, good shit

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,16 @@
 // custom js
 
 $(document).ready(function() {
+// Make the phone and pad screen invisible if the first project is not responsive
+var firstImg = $('.desktop_slider .slick_slider').find('img').first();
+var responsiveDevices = $('.pad, .phone');
+if ( firstImg.data('res') ) {
+	console.log ("the first image is responsive");
+} else {
+	console.log ("the first image is not responsive");
+	console.log (responsiveDevices);
+	responsiveDevices.hide();
+}
 // Initiate the slick slider plugin
 	var slickIndex = 1;
 	$('.slick_slider').slick({
@@ -10,12 +20,11 @@ $(document).ready(function() {
 			setTimeout(function(){
 				currentImg = $('.slick-active').find('img');
 				responsive = currentImg.data('res');
-				console.log(responsive);
 				if ( responsive ) {
-					console.log("This is responsive");
+					responsiveDevices.fadeIn('fast');
 				}
 				else {
-					console.log("This is not responsive");
+					responsiveDevices.fadeOut('fast');
 				}
   			}, 1); //end of setTimeout()
 
