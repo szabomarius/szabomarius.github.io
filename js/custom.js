7962dafdadff8077ba46dab73b6a73a3444491b9
Szabo Marius <szabomarius@gmail.com>
2014-04-13 16:38:14 +0300

    FJQWKFLJQKLFJQWKFJ THE SLIDER HAS NO CURRENT SLIDE EXCEPT A DAMN CLASS AND THE CLASS IS BEING APPLIED AFTER THE CALLBACK HAS BEEN TRIGGERED, SO I GOD DAMN HAD TO PUT A TIMEOUT FUNCTION FOR THE CODE TO TRIGGER AFTER THE CLASS HAS BEEN APPLIED

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,26 @@
 // custom js
 
-// Initiate the slick slider plugin
 $(document).ready(function() {
+// Initiate the slick slider plugin
+	var slickIndex = 1;
 	$('.slick_slider').slick({
 
 		onAfterChange: function(){
-			var currentImg = $('.slick-active').find('img');
-			console.log(currentImg.data('res'));
+			// Had to put in a timeour because the callback triggered faster than the active class could be applied
+			setTimeout(function(){
+				currentImg = $('.slick-active').find('img');
+				responsive = currentImg.data('res');
+				console.log(responsive);
+				if ( responsive ) {
+					console.log("This is responsive");
+				}
+				else {
+					console.log("This is not responsive");
+				}
+  			}, 1); //end of setTimeout()
 
-			if ( currentImg.attr('class') ) {
 
-			};
-		}
+		} //end of onAfterChange	
 
-	});
-});
+	}); //end of slick()
+}); //end of document.ready()
