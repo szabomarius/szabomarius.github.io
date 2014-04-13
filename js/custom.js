8bfabc9fb3ea6b053a2b8bbd0a673fa59298f60a
Szabo Marius <szabomarius@gmail.com>
2014-04-13 15:23:41 +0300

    Added a callback that finds the attr of the current image

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,17 @@
 // custom js
+
+// Initiate the slick slider plugin
 $(document).ready(function() {
-	$('.slick_slider').slick();
+	$('.slick_slider').slick({
+
+		onAfterChange: function(){
+			var currentImg = $('.slick-active').find('img');
+			console.log(currentImg.data('res'));
+
+			if ( currentImg.attr('class') ) {
+
+			};
+		}
+
+	});
 });
