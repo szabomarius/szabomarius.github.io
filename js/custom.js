f3d56b5c1c552a72c1bfd67cad2a24b2571928e6
Szabo Marius <szabomarius@gmail.com>
2014-05-26 22:22:34 +0300

    made some variables for the slide jquery objects

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,10 @@ $(document).ready(function() {
 
 // Javascript for the web slider
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+// Variables with each jquery slider object
+var webSlider = $('.web_container .slick_slider');
+var logoSlider = $('.logo_container .slick_slider');
+var graphicSlider = $('.graphic_container .slick_slider');
 // Make the phone and pad screen invisible if the first project is not responsive
 var firstImg = $('.desktop_slider .slick_slider').find('img').first();
 var responsiveDevices = $('.pad, .phone');
@@ -16,73 +19,73 @@ if ( firstImg.data('res') ) {
 	responsiveDevices.hide();
 }
 // Initiate the slick slider plugin
-	$('.web_container .slick_slider').slick({
-		speed: 800 ,
+webSlider.slick({
+	speed: 800 ,
 
-		onAfterChange: function(){
-			// Had to put in a timeour because the callback triggered faster than the active class could be applied
-			setTimeout(function(){
-				var currentImg = $('.web_container .slick-active').find('img');
-				var responsive = currentImg.data('res');
-				var projectNr = currentImg.data('proj');
-				var link = $('.web_link');
+	onAfterChange: function(){
+		// Had to put in a timeour because the callback triggered faster than the active class could be applied
+		setTimeout(function(){
+			var currentImg = $('.web_container .slick-active').find('img');
+			var responsive = currentImg.data('res');
+			var projectNr = currentImg.data('proj');
+			var link = $('.web_link');
 
-				// for responsive images only 
-				if ( responsive ) {
-					responsiveDevices.fadeIn('fast');
-					// get the images for responsive devices
-					var project = currentImg.data('proj');
-					switch (project) {
-						case "project1":
-							$(".pad img").fadeTo(100,0.50, function() {
-								$(this).attr("src" , "img/slider/web/project1-pad.jpg");
-							}).fadeTo(100,1);
-							$(".phone img").fadeTo(80,0.50, function() {
-								$(this).attr("src" , "img/slider/web/project1-phone.jpg");
-							}).fadeTo(100,1);
-							break;
-						case "project3":
-							$(".pad img").fadeTo(80,0.50, function() {
-								$(this).attr("src" , "img/slider/web/project3-pad.jpg");
-							}).fadeTo(100,1);
-							$(".phone img").fadeTo(80,0.50, function() {
-								$(this).attr("src" , "img/slider/web/project3-phone.jpg");
-							}).fadeTo(100,1);
-							break;
-					} // end switch
-				}
-				else {
-					responsiveDevices.fadeOut('fast');
-				} // end if else
-
-				// Change links according to the current selected
-				switch (projectNr) {
+			// for responsive images only 
+			if ( responsive ) {
+				responsiveDevices.fadeIn('fast');
+				// get the images for responsive devices
+				var project = currentImg.data('proj');
+				switch (project) {
 					case "project1":
-						link.attr("href" , "http://www.google-1.com");
-						break;
-					case "project2":
-						link.attr("href" , "http://www.google-2.com");
+						$(".pad img").fadeTo(100,0.50, function() {
+							$(this).attr("src" , "img/slider/web/project1-pad.jpg");
+						}).fadeTo(100,1);
+						$(".phone img").fadeTo(80,0.50, function() {
+							$(this).attr("src" , "img/slider/web/project1-phone.jpg");
+						}).fadeTo(100,1);
 						break;
 					case "project3":
-						link.attr("href" , "http://www.google-3.com");
+						$(".pad img").fadeTo(80,0.50, function() {
+							$(this).attr("src" , "img/slider/web/project3-pad.jpg");
+						}).fadeTo(100,1);
+						$(".phone img").fadeTo(80,0.50, function() {
+							$(this).attr("src" , "img/slider/web/project3-phone.jpg");
+						}).fadeTo(100,1);
 						break;
-					default:
-						
-				}
+				} // end switch
+			}
+			else {
+				responsiveDevices.fadeOut('fast');
+			} // end if else
 
-  			}, 1); //end of setTimeout()
+			// Change links according to the current selected
+			switch (projectNr) {
+				case "project1":
+					link.attr("href" , "http://www.google-1.com");
+					break;
+				case "project2":
+					link.attr("href" , "http://www.google-2.com");
+					break;
+				case "project3":
+					link.attr("href" , "http://www.google-3.com");
+					break;
+				default:
+					
+			}
+
+			}, 1); //end of setTimeout()
 
 
-		} //end of onAfterChange	
+	} //end of onAfterChange	
 
-	}); //end of slick()
+}); //end of slick()
 
 // Javascript for the logo slider
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // Initiate the slick slider plugin
 
-$('.logo_container .slick_slider').slick({
+logoSlider.slick({
 	speed: 500 ,
 
 	onAfterChange: function(){
@@ -100,7 +103,7 @@ $('.logo_container .slick_slider').slick({
 // Javascript for the graphic slider
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-$('.graphic_container .slick_slider').slick({
+graphicSlider.slick({
 	speed: 500
 })
 
